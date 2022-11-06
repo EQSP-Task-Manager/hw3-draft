1aa6e5a83232ccc6eea09f2cb5406a272d818785
Maxim Pryanikov <mx.pryanikov@gmail.com>
2022-11-06 18:11:22 +0300

    Changes for tests to fail

--- internal/api/handlers.py
+++ internal/api/handlers.py
@@ -4,4 +4,4 @@ from aiohttp import web
 class PingHandler(web.View):
 
     async def get(self) -> web.Response:
-        return web.Response(status=web.HTTPOk.status_code)
+        return web.Response(status=web.HTTPInternalServerError.status_code)
